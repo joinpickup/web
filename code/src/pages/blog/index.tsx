29ef8dd8c8b847cac1889c8693c3846cb1a0f5bc
andrew cohen <cohen.andrew.n@gmail.com>
2022-10-31 21:40:15 -0400

    start working connecting to notion

--- code/src/pages/blog/index.tsx
+++ code/src/pages/blog/index.tsx
@@ -1,17 +1,28 @@
 import {NextPage} from "next";
+import {getDatabase} from "../../lib/notion";
 import {Navbar} from "../index";
 
-const Company: NextPage = () => {
+export async function getServerSideProps() {
+  const database = await getDatabase(process.env.NOTION_PAGE_ID as string)
+  console.log()
+
+  return {
+    props: {
+      posts: database,
+    }
+  }
+}
+
+const Blog: NextPage = ({ posts }: any) => {
   return (
     <>
       <Navbar />
       <main className="container mx-auto flex min-h-screen flex-col p-4 space-y-4">
         <div className="flex flex-col justify-center items-center w-full space-y-4">
-          <div className="flex md:text-6xl text-3xl">Coming Soon</div>
         </div>
       </main>
     </>
   )
 }
 
-export default Company;
+export default Blog;
