8ca12830dbf85a78208562cecdafe3810f990857
andrew cohen <cohen.andrew.n@gmail.com>
2022-12-13 23:52:24 -0500

    feat: add analytics

--- code/src/pages/index.tsx
+++ code/src/pages/index.tsx
@@ -7,6 +7,7 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Pickup</title>
+        <script defer data-domain="joinpickup.com" src="https://analytics.joinpickup.com/js/script.js"></script>
         <meta name="description" content="Landing page for pickup" />
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;500;700&display=swap" rel="stylesheet" />
