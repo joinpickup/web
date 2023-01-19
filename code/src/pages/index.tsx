68a67b4c630b5a04e265a47fd57ac6d13f4714f9
andrew cohen <cohen.andrew.n@gmail.com>
2023-01-18 22:31:05 -0500

    feat: add meta tags

--- code/src/pages/index.tsx
+++ code/src/pages/index.tsx
@@ -10,13 +10,17 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Pickup</title>
+        <title>Pickup LLC</title>
         <script
           defer
           data-domain="joinpickup.com"
           src="https://analytics.joinpickup.com/js/script.js"
         ></script>
-        <meta name="description" content="Landing page for pickup" />
+        <meta property="og:title" content="Pickup LLC"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://joinpickup.com/pickup-logo-v2.png"/>
+        <meta property="og:url" content="https://joinpickup.com"/>
+        <meta name="description" content="Community like you've never seen before." />
         <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;500;700&display=swap"
