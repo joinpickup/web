66bc3d150051f3117f89cff92cd501adcf66d805
andrew cohen <cohen.andrew.n@gmail.com>
2023-01-23 19:08:08 -0500

    feat: add content back

---
+++ code/src/pages/_document.tsx
@@ -0,0 +1,43 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;500;700&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+          <script
+            defer
+            data-domain="joinpickup.com"
+            src="https://analytics.joinpickup.com/js/script.js"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
