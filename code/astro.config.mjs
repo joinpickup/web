cdd7fcf6393ebd358acc74a1d16a764b54a28964
andrew cohen <cohen.andrew.n@gmail.com>
2023-07-18 21:01:01 -0400

    feat: begin working on framework

--- code/astro.config.mjs
+++ code/astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vercelStatic from '@astrojs/vercel/static';
- 
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: vercelStatic(),
+  integrations: [tailwind()]
 });
